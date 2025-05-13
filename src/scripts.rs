1f492e67f3f981baa0d8fd570233fdeb8d282102
._______166 <retromac9x2@gmail.com>
2025-05-13 22:07:51 +0100

    remove unused function

--- src/scripts.rs
+++ src/scripts.rs
@@ -20,24 +20,3 @@ pub fn read_common_sh() -> (String, String, String) {
 
     (aosp_tag, aosp_tag_old, branch)
 }
-
-pub fn read_config_file() -> (String, String, String) {
-    const CONFIG_PATH: &str = "common";
-    let path = env::current_exe().unwrap().parent().unwrap().join(CONFIG_PATH);
-    let contents = fs::read_to_string(path).expect("Failed to read common.sh");
-    let mut graphene_tag = String::new();
-    let mut graphene_tag_old = String::new();
-    let mut cm_latest_branch = String::new();
-
-    for line in contents.lines() {
-        if line.starts_with("readonly graphene_tag=") {
-            graphene_tag = line.trim_start_matches("readonly graphene_tag=").trim_matches('"').to_string();
-        } else if line.starts_with("readonly graphene_tag_old=") {
-            graphene_tag_old = line.trim_start_matches("readonly graphene_tag_old=").trim_matches('"').to_string();
-        } else if line.starts_with("readonly cm_latest_branch=") {
-            cm_latest_branch = line.trim_start_matches("readonly cm_latest_branch=").trim_matches('"').to_string();
-        }
-    }
-
-    (graphene_tag, graphene_tag_old, cm_latest_branch)
-}
